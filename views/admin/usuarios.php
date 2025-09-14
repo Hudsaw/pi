9d39a6dc963020af482b2fc596411d302914eebd
Maria <tonhapaino25@gmail.com>
2025-09-14 20:58:03 -0300

    Começado a implementar tabela para listagem de usuarios

--- views/admin/usuarios.php
+++ views/admin/usuarios.php
@@ -3,10 +3,38 @@
         <a class="item bold">Usuários</a>
         <a href="<?= BASE_URL ?>/" class="sair">Sair</a>
     </div>
-    <div class="cards">
-        <a href="<?= BASE_URL ?>admin/criar-usuario" class="card novo-usuario">Criar usuário</a><!-- Fazer ícone -->
+    <!-- <div class="cards">
+        <a href="<?= BASE_URL ?>admin/criar-usuario" class="card novo-usuario">Criar usuário</a>
         <?php foreach ($listaUsuarios as $usuario): ?>
             <a href="<?= BASE_URL ?>admin/visualizar-usuario?id=<?= $usuario['id'] ?>" class="card"><?= htmlspecialchars($usuario['nome']) ?><br><br><?= htmlspecialchars($usuario['especialidade']) ?></a>
         <?php endforeach; ?>
+    </div> -->
+    <div class="tabela">
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Telefone</th>
+                    <th>CPF</th>
+                    <th>Cidade</th>
+                    <th>Visualizar</th>
+                    <th>Editar</th>
+                    <th>Remover</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($listaUsuarios as $usuario): ?>
+                    <tr>
+                        <td><?= htmlspecialchars($usuario['nome']) ?></td>
+                        <td><?= htmlspecialchars($usuario['telefone']) ?></td>
+                        <td><?= htmlspecialchars($usuario['cpf']) ?></td>
+                        <td><?= htmlspecialchars($usuario['cidade']) ?></td>
+                        <td><a href="<?= BASE_URL ?>admin/visualizar-usuario?id=<?= $usuario['id'] ?>">Visualizar</a></td>
+                        <td>Editar</td>
+                        <td>Remover</td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
 </div>
